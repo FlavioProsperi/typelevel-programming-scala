b04698844a1997a19b397708b334d6f2aaf7ed6b
Leszek Gruchała <leszek@gruchala.eu>
2016-03-14 21:14:43 +0100

    Added resources

--- src/main/scala/eu/gruchala/typelevel/full/A_About.scala
+++ src/main/scala/eu/gruchala/typelevel/full/A_About.scala
@@ -6,8 +6,29 @@ object A_About {
   val desc =
     """
       |TypeLevel Programming allows to put additional constraints on code on compile time level.
+      |Allows to define various kinds of our types.
       |Results in type safety and less errors and mistakes while writing code, not running it.
       |With higher order functions and implicits allows to write concise, abstract and reusable code.
+    """.stripMargin
+
+  val resources =
+    """
+      |Aux Pattern
+      |https://gigiigig.github.io/posts/2015/09/13/aux-pattern.html
+      |
+      |Magnet pattern
+      |http://spray.io/blog/2012-12-13-the-magnet-pattern/
+      |
+      |Phantom Types
+      |https://blog.codecentric.de/en/2016/02/phantom-types-scala/
+      |
+      |Functors / Monads / Applicatives / Category Theory
+      |https://thedet.wordpress.com/2012/04/28/functors-monads-applicatives-can-be-so-simple/
+      |http://www.casualmiracles.com/2012/01/08/a-small-functor-example/
+      |https://hseeberger.wordpress.com/2011/01/31/applicatives-are-generalized-functors/
+      |https://hseeberger.wordpress.com/2010/11/25/introduction-to-category-theory-in-scala/
+      |https://vimeo.com/28793245
+      |
     """.stripMargin
 
 }
